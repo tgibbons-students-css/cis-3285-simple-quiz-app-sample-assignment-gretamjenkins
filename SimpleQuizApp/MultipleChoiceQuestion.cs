8f946d246ed0fd60c0ef6fb05bc2b2158bc71323
Greta Jenkins <gjenkins@css.edu>
2018-09-18 10:40:01 -0500

    Copied and pasted quiz app class files from last week's participation assignment

---
+++ SimpleQuizApp/MultipleChoiceQuestion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleQuizApplication
+{
+    class MultipleChoiceQuestion : Question
+    {
+        string[] possibleAnswers;
+        int answerIndex;
+
+        public MultipleChoiceQuestion(string questionText, string[] possibleAnswers, int answerIndex) : base(questionText)
+        {
+            this.possibleAnswers = possibleAnswers;
+            this.answerIndex = answerIndex;
+        }
+
+        /// getQuestion()
+        /// @return Adds newline after each possible answer
+        public override string getQuestion()
+        {
+            string fullQuestion = questionText + Environment.NewLine;
+            foreach (string s in possibleAnswers)
+            {
+                fullQuestion += s + Environment.NewLine;
+            }
+            return fullQuestion;
+        }
+
+        public override string getAnswer()
+        {
+            return possibleAnswers[answerIndex];
+        }
+
+        public override bool checkAnswer(string answer)
+        {
+            return (answer.Equals(possibleAnswers[answerIndex], StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
--- SimpleQuizApp/ShortAnswerQuestion.cs
+++ SimpleQuizApp/ShortAnswerQuestion.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SimpleQuizApplication
 {
-    public class ShortAnswerQuestion : Question
+    class ShortAnswerQuestion : Question
     {
         private String answer;
 
@@ -39,16 +39,5 @@ namespace SimpleQuizApplication
             return (answer.Equals(givenAnswer, StringComparison.InvariantCultureIgnoreCase));
         }
 
-        /// <summary>
-        /// The function removes all whitespace from a string. So it removes all spaces at the start, middle and end of a string
-        ///  ***** Currently this method is never called so a unit test should fail *****
-        /// </summary>
-        /// <param name="fullString">The given string where whitespaces will be removed</param>
-        /// <returns>The given string with all white space removed</returns>
-        private String RemoveWhitespace(String fullString)
-        { 
-            String resultString = String.Concat(fullString.Where(c => !char.IsWhiteSpace(c)));
-            return resultString;
-        }
     }
 }
