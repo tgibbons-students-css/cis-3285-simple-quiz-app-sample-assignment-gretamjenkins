767feb7df98871f1489609aa395981007eabdaa1
Greta Jenkins <gjenkins@css.edu>
2018-09-18 10:48:38 -0500

    Shuffle method copied and uncommented the remaining 3 questions

--- SimpleQuizApp/SimpleQuizModel.cs
+++ SimpleQuizApp/SimpleQuizModel.cs
@@ -28,23 +28,52 @@ namespace SimpleQuizApplication
                     "What is the name of Jerry Lee Lewis's biggest solid gold hit?",
                     "Great Balls of Fire"));
             //ADD MORE EXAMPLE QUESTIONS HERE
-            /*
-            myQuestions.add(new ShortAnswerQuestion(
+            myQuestions.Add(new ShortAnswerQuestion(
                     "What is the Minnesota state bird?",
                     "Loon"));
-            myQuestions.add(new TrueFalseQuestion(
+            myQuestions.Add(new TrueFalseQuestion(
                    "St. Scholatsica is over 100 years old",
                    "True"));
             String[] possibleAnswers = { "Dave Vosen", "Jen Rosato", "Tom Buck", "Tom Gibbons" };
-            myQuestions.add(new MultipleChoiceQuestion(
+            myQuestions.Add(new MultipleChoiceQuestion(
                     "Which faculty member owns a number of bee hives?",
                     possibleAnswers,
                     2));
-            */
 
             //Shuffle the questions and set the starting one to zero
-            // Collections.shuffle(myQuestions);            // C# does not have a clear shuffle option
+            myQuestions = Shuffle(myQuestions);
             currentQuestion = 0;
+        }
+
+        /// <summary>
+        /// from http://www.rudrasofttech.com/blog/how-to-shuffle-or-randomize-a-generic-list-using-csharp
+        /// </summary>
+        /// <typeparam name="t"></typeparam>
+        /// <param name="list"></param>
+        /// <returns>shuffledList</returns>
+        public List<t> Shuffle<t>(List<t> list)
+        {
+            var rnd = new Random();
+
+            List<t> shuffledList = new List<t>();
+
+            //make a list of the indexes for list
+            int listLength = list.Count;
+
+            for (int i = 0; i < listLength; i++)
+            {
+
+                //get a random index from the list of index
+                int index = rnd.Next(0, list.Count);
+
+                //pick the object from list based on index value stored in list of indexes
+                shuffledList.Add(list[index]);
+
+                //remove the selected index from list of indexes
+                list.RemoveAt(index);
+            }
+
+            return shuffledList;
         }
 
         /**
